6423f089fc480074928331e89b2b1dfc0103e9b6
Christopher R. Peterson <Christopher.R.Peterson@gmail.com>
2020-12-07 20:38:39 -0600

    Updated prep_runs.R w/ genus info (tentative)
    Double check this

--- R/prep_runs.r
+++ R/prep_runs.r
@@ -14,12 +14,18 @@ dir.create("out/model_runs")
 dir.create("out/loo")
 ## Create a list of all predictor combinations ========
 terms = c("cop.lake", "worm.lake", "worm.lake:cop.lake", 
+          "genus", "genus:worm.lake", # We're explicitly identifying the interactions here so they have the possibility of being included
           "native", "(1|plate)", "(1|worm.fam)")
+
 ## Removes combinations where interaction is present 
 ## without corresponding main effect
 fix_interaction = function(arg) {
   if("worm.lake:cop.lake" %in% arg) {
     if(! (("cop.lake" %in% arg) && ("worm.lake" %in% arg)))
+      arg = "" # empty string will later be filtered out
+  } # copy this nested if statement & make it work for genus:worm.lake
+  if("worm.lake:genus" %in% arg) {
+    if(! (("genus" %in% arg) && ("worm.lake" %in% arg)))
       arg = ""
   }
   arg
@@ -33,11 +39,11 @@ pick_n_predictors = function(n, preds = terms) {
 }
 
 rhs_combos = 
-  map(seq_along(terms), pick_n_predictors) %>%  # 1 - 6 possible predictors in a model
+  map(seq_along(terms), pick_n_predictors) %>%  # 1 - 8 possible predictors in a model
   flatten %>% map(fix_interaction) %>%
   # Convert to character version of a formula
   map_chr(~paste("~", paste(.x, collapse = " + "))) %>% 
-  remove_null
+  remove_null()
 # Since this is a two part model, we need all 2-part combos of the above
 model_formulas_txt = 
   list(pois = paste("numb.worm", rhs_combos),
@@ -52,8 +58,8 @@ model_formulas = model_formulas_txt %>%
 determine_prior = function(.x){
   re_hurdle = grepl("plate", x = .x$hu, fixed = TRUE) | grepl("worm.fam", x = .x$hu, fixed = TRUE)
   re_pois = grepl("plate", x = .x$pois, fixed = TRUE) | grepl("worm.fam", x = .x$pois, fixed = TRUE)
-  b_hurdle = grepl("lake", x = .x$hu, fixed = TRUE)|grepl("native", x = .x$hu, fixed = TRUE)
-  b_pois = grepl("lake", x = .x$pois, fixed = TRUE)|grepl("native", x = .x$pois, fixed = TRUE)
+  b_hurdle = grepl("lake", x = .x$hu, fixed = TRUE)|grepl("native", x = .x$hu, fixed = TRUE) | grepl("genus", x = .x$hu, fixed = TRUE)
+  b_pois = grepl("lake", x = .x$pois, fixed = TRUE)|grepl("native", x = .x$pois, fixed = TRUE)  | grepl("genus", x = .x$hu, fixed = TRUE)
   
   prior = prior_string("normal(0, 6)", class = "Intercept") #the prior in the intercept always has to be bigger than any other priors below 
   if(isTRUE(re_hurdle)) prior = prior + prior_string("student_t(7, 0, 1)", class = "sd", dpar = "hu")
